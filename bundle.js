ab33b57a961eaab1e269eb95efca0c122c43214e
Peter Gessler <gessler.pd@gmail.com>
2017-04-10 09:22:15 -0500

    Closes #1 + audio controls

--- bundle.js
+++ bundle.js
@@ -1,31 +1,32 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-/* global Audio */
-
 const regl = require('regl')()
+const analyse = require('web-audio-analyser')
+const soundcloudBadge = require('soundcloud-badge')
 const camera = require('regl-camera')(regl, {
   center: [0, 0, 0],
   damping: 0
 })
 
-const analyse = require('web-audio-analyser')
-
-require('soundcloud-badge')({
+soundcloudBadge({
   client_id: '4027e272825f07badf19f66d7827a79f',
   song: 'https://soundcloud.com/khamsinmusic/maelstrom',
   dark: false,
   getFonts: false
-}, function (err, src) {
-  if (err) throw err
-  var audio = new Audio()
-  audio.crossOrigin = 'anonymous'
-  audio.src = src
-  audio.loop = true
-  var analyzer = analyse(audio, { audible: true, stereo: false })
+},
+  function (err, src, data, div) {
+    if (err) throw err
+    var audio = document.createElement('audio')
+    audio.classList.add('player')
+    audio.crossOrigin = 'anonymous'
+    audio.src = src
+    audio.controls = true
+    audio.loop = true
+    document.body.appendChild(audio)
 
-  audio.play()
+    var analyzer = analyse(audio, { audible: true, stereo: false })
 
-  var drawWaveform = regl({
-    vert: `
+    var drawWaveform = regl({
+      vert: `
     precision mediump float;
     attribute vec3 xyz;
     uniform mat4 projection, view;
@@ -33,22 +34,22 @@ require('soundcloud-badge')({
       gl_Position = projection * view * vec4(xyz,1);
     }
   `,
-    frag: `
+      frag: `
     void main () {
       gl_FragColor = vec4(0,0.3333,1,1);
     }
   `,
-    attributes: {
-      xyz: () => Array.from(analyzer.waveform()).map((freq, i) => {
-        return [0, (freq - 128) / 128, i / 1023 * 2 - 1]
-      })
-    },
-    primitive: 'line strip',
-    count: 1024
-  })
+      attributes: {
+        xyz: () => Array.from(analyzer.waveform()).map((freq, i) => {
+          return [0, (freq - 128) / 128, i / 1023 * 2 - 1]
+        })
+      },
+      primitive: 'line strip',
+      count: 1024
+    })
 
-  var drawFrequency = regl({
-    vert: `
+    var drawFrequency = regl({
+      vert: `
     precision mediump float;
     attribute vec3 xyz;
     uniform mat4 projection, view;
@@ -56,28 +57,28 @@ require('soundcloud-badge')({
       gl_Position = projection * view * vec4(xyz,1);
     }
   `,
-    frag: `
+      frag: `
     void main () {
       gl_FragColor = vec4(1,0,0.2156,1);
     }
   `,
-    attributes: {
-      xyz: () => Array.from(analyzer.frequencies()).map((freq, i) => {
-        return [(freq) / 512, 0, i / 1023 * 2 - 1]
-      })
-    },
-    primitive: 'line strip',
-    count: 1024
-  })
+      attributes: {
+        xyz: () => Array.from(analyzer.frequencies()).map((freq, i) => {
+          return [(freq) / 512, 0, i / 1023 * 2 - 1]
+        })
+      },
+      primitive: 'line strip',
+      count: 1024
+    })
 
-  regl.frame(() => {
-    regl.clear({ color: [0, 0, 0, 1] })
-    camera(() => {
-      drawWaveform()
-      drawFrequency()
+    regl.frame(() => {
+      regl.clear({ color: [0, 0, 0, 1] })
+      camera(() => {
+        drawWaveform()
+        drawFrequency()
+      })
     })
   })
-})
 
 },{"regl":18,"regl-camera":17,"soundcloud-badge":19,"web-audio-analyser":22}],2:[function(require,module,exports){
 
--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-/* global Audio */
-
 const regl = require('regl')()
+const analyse = require('web-audio-analyser')
+const soundcloudBadge = require('soundcloud-badge')
 const camera = require('regl-camera')(regl, {
   center: [0, 0, 0],
   damping: 0
 })
 
-const analyse = require('web-audio-analyser')
-
-require('soundcloud-badge')({
+soundcloudBadge({
   client_id: 'YOUR_SOUNDCLOUD_CLIENTID',
   song: 'https://soundcloud.com/khamsinmusic/maelstrom',
   dark: false,
   getFonts: false
-}, function (err, src) {
-  if (err) throw err
-  var audio = new Audio()
-  audio.crossOrigin = 'anonymous'
-  audio.src = src
-  audio.loop = true
-  var analyzer = analyse(audio, { audible: true, stereo: false })
+},
+  function (err, src, data, div) {
+    if (err) throw err
+    var audio = document.createElement('audio')
+    audio.classList.add('player')
+    audio.crossOrigin = 'anonymous'
+    audio.src = src
+    audio.controls = true
+    audio.loop = true
+    document.body.appendChild(audio)
 
-  audio.play()
+    var analyzer = analyse(audio, { audible: true, stereo: false })
 
-  var drawWaveform = regl({
-    vert: `
+    var drawWaveform = regl({
+      vert: `
     precision mediump float;
     attribute vec3 xyz;
     uniform mat4 projection, view;
@@ -32,22 +33,22 @@ require('soundcloud-badge')({
       gl_Position = projection * view * vec4(xyz,1);
     }
   `,
-    frag: `
+      frag: `
     void main () {
       gl_FragColor = vec4(0,0.3333,1,1);
     }
   `,
-    attributes: {
-      xyz: () => Array.from(analyzer.waveform()).map((freq, i) => {
-        return [0, (freq - 128) / 128, i / 1023 * 2 - 1]
-      })
-    },
-    primitive: 'line strip',
-    count: 1024
-  })
+      attributes: {
+        xyz: () => Array.from(analyzer.waveform()).map((freq, i) => {
+          return [0, (freq - 128) / 128, i / 1023 * 2 - 1]
+        })
+      },
+      primitive: 'line strip',
+      count: 1024
+    })
 
-  var drawFrequency = regl({
-    vert: `
+    var drawFrequency = regl({
+      vert: `
     precision mediump float;
     attribute vec3 xyz;
     uniform mat4 projection, view;
@@ -55,25 +56,25 @@ require('soundcloud-badge')({
       gl_Position = projection * view * vec4(xyz,1);
     }
   `,
-    frag: `
+      frag: `
     void main () {
       gl_FragColor = vec4(1,0,0.2156,1);
     }
   `,
-    attributes: {
-      xyz: () => Array.from(analyzer.frequencies()).map((freq, i) => {
-        return [(freq) / 512, 0, i / 1023 * 2 - 1]
-      })
-    },
-    primitive: 'line strip',
-    count: 1024
-  })
+      attributes: {
+        xyz: () => Array.from(analyzer.frequencies()).map((freq, i) => {
+          return [(freq) / 512, 0, i / 1023 * 2 - 1]
+        })
+      },
+      primitive: 'line strip',
+      count: 1024
+    })
 
-  regl.frame(() => {
-    regl.clear({ color: [0, 0, 0, 1] })
-    camera(() => {
-      drawWaveform()
-      drawFrequency()
+    regl.frame(() => {
+      regl.clear({ color: [0, 0, 0, 1] })
+      camera(() => {
+        drawWaveform()
+        drawFrequency()
+      })
     })
   })
-})
